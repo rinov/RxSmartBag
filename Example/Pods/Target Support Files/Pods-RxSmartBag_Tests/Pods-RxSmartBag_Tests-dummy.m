f88d9a81ae56db232fc9b10c110f65d300e29129
rinov <rinov@rinov.jp>
2017-05-09 11:53:44 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-RxSmartBag_Tests/Pods-RxSmartBag_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSmartBag_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSmartBag_Tests
+@end
---
+++ Example/Pods/Target Support Files/RxSmartBag/RxSmartBag-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxSmartBag : NSObject
+@end
+@implementation PodsDummy_RxSmartBag
+@end
